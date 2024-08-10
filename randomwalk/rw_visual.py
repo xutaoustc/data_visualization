4137841b9d11ae563025faebd740c064eeee320a
xutao <xutao_ustc@163.com>
2024-08-10 22:04:02 +0800

    random walk

---
+++ randomwalk/rw_visual.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.style.use("classic")
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+ax.set_aspect("equal")
+plt.show()
